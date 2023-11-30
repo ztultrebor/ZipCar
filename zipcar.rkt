fb42e2e286a5e7fe02295d493af7b9c3648d80d1
Robert Lutz <robertlutzi@gmail.com>
2023-11-30 13:49:51 -0500

    fixed stop-when conditions

--- zipcar.rkt
+++ zipcar.rkt
@@ -4,8 +4,6 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-; WorldState: clock ticks elapsed
-
 ; constants
 (define WORLDWIDTH 200) ; horizontal world size
 (define WORLDHEIGHT 25) ; vertical world size
@@ -14,6 +12,7 @@
 (define CARLENGTH (* 2 WHEELGAP))
 (define Y-OFFSET (- WORLDHEIGHT (* 2 WHEELRADIUS))) ; y-pos of car's center
 (define SPEED 3) ; pixels per tick
+(define CYCLES 2); stops after this many
 
 (define CANVAS (empty-scene WORLDWIDTH WORLDHEIGHT))
 (define BOUGHS (circle 8 "solid" "green"))
@@ -27,14 +26,17 @@
 (define CAR (underlay/xy FRAME WHEELRADIUS (* 2 WHEELRADIUS) CHASSIS)) ; the whole car
 (define BACKDROP (place-image TREE 150 11 CANVAS)) ; canvas with tree
 
+
 ;functions
+; WorldState: clock ticks elapsed
+
 ; WorldState -> INT
 ; produce new position of car's front end after clock tick
 (check-within (position 0) CARLENGTH 1/1000000) ; checks
 (check-within (position (/ WORLDWIDTH  SPEED 2)) WORLDWIDTH 1/1000000) ; checks
 (define (position t) (/ (+ WORLDWIDTH CARLENGTH
-                        (* (- WORLDWIDTH CARLENGTH)
-                           (sin (- (/ (* 2 pi SPEED t) WORLDWIDTH) (/ pi 2)))))
+                           (* (- WORLDWIDTH CARLENGTH)
+                              (sin (- (/ (* 2 pi SPEED t) WORLDWIDTH) (/ pi 2)))))
                         2))
 
 ; WorldState -> IMG
@@ -44,10 +46,10 @@
 (define (render t) (place-image CAR (- (position t) WHEELGAP) Y-OFFSET BACKDROP))
 
 ; WorldState -> BOOL
-; quits when car completely exits stage left
-(check-expect (finished? (/ WORLDWIDTH SPEED)) #false) ; checks
-(check-expect (finished? (/ (+ WORLDWIDTH CARLENGTH 1) SPEED)) #true) ; checks
-(define (finished? t) (> (position t) (+ WORLDWIDTH CARLENGTH)))
+; quits after CYCLES cycles
+(check-expect (finished? (/ (* WORLDWIDTH CYCLES) SPEED)) #true) ; checks
+(check-expect (finished? (- (/ (* WORLDWIDTH CYCLES) SPEED) 1)) #false) ; checks
+(define (finished? t) (>= (/ (* SPEED t) WORLDWIDTH) CYCLES))
 
 ; WorldState -> WorldState
 ; launches the program from some initial state 
@@ -59,6 +61,7 @@
     )
   )
 
+
 ; actions!
 
-(main 1)
+(main 0)
